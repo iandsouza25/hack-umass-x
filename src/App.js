3d2fb36b389695a71cda0a652d0afdeddbcbe028
Ian D'Souza <dsouza.ianp@gmail.com>
2022-11-12 17:05:49 -0500

    got rid of bad words and added different arrays

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import React from "react";
 function App() {
   const data = require("./words_dictionary.json");
   let wordArr = Object.keys(data);
-
-  function selectWords(wordArr) {
-    let returnArr = [];
-    let indTracker = new Set();
-    while (returnArr.length < 50) {
-      let indVal = Math.floor(Math.random() * wordArr.length);
-      if (!indTracker.has(indVal)) {
-        returnArr.push(wordArr[indVal]);
+  let easyWords = wordArr.filter(x => x.length<=5);
+  let mediumWords = wordArr.filter(x => x.length<=8)
+  let hardWords = wordArr.filter(x => x.length>=10);
+  
+  function selectWords(wordArr){
+      let returnArr =[];
+      let indTracker = new  Set();
+      while (returnArr.length<50){
+          let indVal = Math.floor(Math.random()*wordArr.length);
+          if (!indTracker.has(indVal)){
+              returnArr.push(wordArr[indVal]);
+          }
+          indTracker.add(indVal);
       }
-      indTracker.add(indVal);
-    }
-    return returnArr;
+      return returnArr;
+  }  
+  
+  function arrToString(arr){
+      return arr.join(" ");
   }
 
-  let tempString = selectWords(wordArr).join(" ");
+  let tempString= arrToString(selectWords(easyWords));
 
   return (
     <div>
