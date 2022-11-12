cd167793e273d4d20d3d31f8cc33c0643b438d2a
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2022-11-12 16:33:59 -0500

    add selectWords

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React from "react";
+
 function App() {
-  return null
+  const data = require("./words_dictionary.json");
+  let wordArr = Object.keys(data);
+
+  function selectWords(wordArr) {
+    let returnArr = [];
+    let indTracker = new Set();
+    while (returnArr.length < 50) {
+      let indVal = Math.floor(Math.random() * wordArr.length);
+      if (!indTracker.has(indVal)) {
+        returnArr.push(wordArr[indVal]);
+      }
+      indTracker.add(indVal);
+    }
+    return returnArr;
+  }
+
+  let tempString = selectWords(wordArr).join(" ");
+
+  return (
+    <div>
+      <button>start</button>
+      <p>{tempString}</p>
+    </div>
+  )
 }
 
 export default App;
